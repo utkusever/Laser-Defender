621bf193376ff72b3c131239937d2efa92d8e085
utkusever <c.utkusever@outlook.com>
2021-11-02 22:12:31 +0300

    Initial commit

---
+++ Laser Defender/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    Material mymaterial;
+    Vector2 offSet;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mymaterial = GetComponent<Renderer>().material;
+        offSet = new Vector2(0f,backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mymaterial.mainTextureOffset += offSet * Time.deltaTime; 
+    }
+}
---
+++ Laser Defender/Assets/Scripts/Level.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Level : MonoBehaviour
+{
+    [SerializeField] float delayInSecond = 1f;
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad());
+        
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSecond);
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
